0b0131d66f881b9184b1c4bacd68b6cfbe3c3880
wyt <l_o_e@163.com>
2025-09-27 17:23:22 +0800

    1.0

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
   base: '/rzsl/', // 重要：GitHub Pages 需要相对路径
   build: {
     outDir: 'dist',
-    emptyOutDir: true, // 构建前清空输出目录
+    emptyOutDir: true // 构建前清空输出目录
   },
   server: {
-    port: 3000,
+    port: 3000
   }
 })
