bfdf8e5440ed1571bd07118a0677f39d4540c630
wyt <l_o_e@163.com>
2025-09-27 17:15:15 +0800

    1.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 重要：GitHub Pages 需要相对路径
+  base: '/rzsl/', // 重要：GitHub Pages 需要相对路径
   build: {
     outDir: 'dist',
     emptyOutDir: true, // 构建前清空输出目录
